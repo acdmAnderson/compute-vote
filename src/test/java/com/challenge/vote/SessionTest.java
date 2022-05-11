7b38a12b69afe370e0886f106f4c9bcaa5637473
andersonma <anderson.mata@calcard.com.br>
2022-05-11 20:13:43 -0300

    test: ensure Session is Open

--- src/test/java/com/challenge/vote/SessionTest.java
+++ src/test/java/com/challenge/vote/SessionTest.java
@@ -5,22 +5,30 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import static java.time.LocalDateTime.parse;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 public class SessionTest {
 
     @Test
     void shouldCreateSession() {
-        final var session = new Session(1L, "ANY_SESSION", 3600L, parse("2020-01-01T10:00:00"));
+        final var session = new Session(1L, "ANY_SESSION", 3600L);
         assertEquals(session.getId(), 1L);
         assertEquals(session.getDescription(), "ANY_SESSION");
         assertEquals(session.getDuration(), 3600L);
-        assertEquals(session.getStartDate(), parse("2020-01-01T10:00:00"));
     }
 
     @Test
     void shouldCreateSessionWithDefaultDuration() {
         final var session = new Session(1L, "ANY_SESSION");
         assertEquals(session.getDuration(), 60L);
+    }
+
+    @Test
+    void shouldOpenSession() {
+        final var session = new Session(1L, "ANY_SESSION", 3600L);
+        session.open(parse("2020-01-01T10:00:00"));
+        final var isOpen = session.isOpen(parse("2020-01-01T10:30:00"));
+        assertTrue(isOpen);
     }
 }
