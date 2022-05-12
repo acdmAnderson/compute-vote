f7415a12cf9f2f2a2eeae05923f7bc34df52809a
andersonma <anderson.mata@calcard.com.br>
2022-05-12 08:11:44 -0300

    test: ensure Vote be registered

---
+++ src/test/java/com/challenge/vote/application/usecases/vote/DoVoteTest.java
@@ -0,0 +1,54 @@
+package com.challenge.vote.application.usecases.vote;
+
+import com.challenge.vote.application.usecases.session.CreateSession;
+import com.challenge.vote.application.usecases.session.CreateSessionInput;
+import com.challenge.vote.infra.repositories.memories.SessionRepositoryMemory;
+import com.challenge.vote.infra.repositories.memories.VoteRepositoryMemory;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class DoVoteTest {
+    @Autowired
+    private SessionRepositoryMemory sessionRepository;
+
+    @Autowired
+    private VoteRepositoryMemory voteRepositoryMemory;
+
+    @BeforeEach
+    void setup() {
+        this.sessionRepository.clean();
+        this.voteRepositoryMemory.clean();
+    }
+
+    @Test
+    void shouldDoVote() throws Exception {
+        final var sessionInput = CreateSessionInput.builder()
+                .sessionId(1L)
+                .description("ANY_SESSION")
+                .duration(3600L)
+                .build();
+        final var createSession = new CreateSession(sessionRepository);
+        createSession.execute(sessionInput);
+        final var doVote = new DoVote(sessionRepository, voteRepositoryMemory);
+        final var firstInputVote = DoVoteInput.builder()
+                .cpf("ANY_CPF")
+                .id(1L)
+                .idSession(1L)
+                .inFavor(Boolean.TRUE)
+                .build();
+        doVote.execute(firstInputVote);
+        final var secondInputVote = DoVoteInput.builder()
+                .cpf("ANOTHER_CPF")
+                .id(2L)
+                .idSession(1L)
+                .inFavor(Boolean.FALSE)
+                .build();
+        doVote.execute(secondInputVote);
+        final var votes = voteRepositoryMemory.findByIdSession(1L);
+        Assertions.assertEquals(votes.size(), 2L);
+    }
+}
