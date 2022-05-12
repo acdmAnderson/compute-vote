ba03ea769403dc648971689a305313bbc8875a38
andersonma <anderson.mata@calcard.com.br>
2022-05-12 13:12:53 -0300

    test: ensure DoVote Throw when session is not open

--- src/test/java/com/challenge/vote/application/usecases/vote/DoVoteTest.java
+++ src/test/java/com/challenge/vote/application/usecases/vote/DoVoteTest.java
@@ -2,8 +2,10 @@ package com.challenge.vote.application.usecases.vote;
 
 import com.challenge.vote.application.usecases.session.CreateSession;
 import com.challenge.vote.application.usecases.session.CreateSessionInput;
+import com.challenge.vote.application.usecases.session.OpenSession;
 import com.challenge.vote.infra.repositories.memories.SessionRepositoryMemory;
 import com.challenge.vote.infra.repositories.memories.VoteRepositoryMemory;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,29 +31,50 @@ public class DoVoteTest {
 
     @Test
     void shouldDoVote() throws Exception {
+        final var createSession = new CreateSession(sessionRepository);
+        final var openSession = new OpenSession(sessionRepository);
+        final var doVote = new DoVote(sessionRepository, voteRepositoryMemory);
         final var sessionInput = CreateSessionInput.builder()
                 .sessionId(1L)
                 .description("ANY_SESSION")
                 .duration(3600L)
                 .build();
-        final var createSession = new CreateSession(sessionRepository);
-        createSession.execute(sessionInput);
-        final var doVote = new DoVote(sessionRepository, voteRepositoryMemory);
         final var firstInputVote = DoVoteInput.builder()
                 .cpf("ANY_CPF")
                 .id(1L)
                 .idSession(1L)
                 .inFavor(TRUE)
                 .build();
-        doVote.execute(firstInputVote);
         final var secondInputVote = DoVoteInput.builder()
                 .cpf("ANOTHER_CPF")
                 .id(2L)
                 .idSession(1L)
                 .inFavor(FALSE)
                 .build();
+        final var session = createSession.execute(sessionInput);
+        openSession.execute(session.getSessionId());
+        doVote.execute(firstInputVote);
         doVote.execute(secondInputVote);
         final var votes = voteRepositoryMemory.findByIdSession(1L);
         assertEquals(votes.size(), 2L);
+    }
+
+    @Test
+    void shouldThrow_whenSessionIsNotOpen() throws Exception {
+        final var createSession = new CreateSession(sessionRepository);
+        final var doVote = new DoVote(sessionRepository, voteRepositoryMemory);
+        final var sessionInput = CreateSessionInput.builder()
+                .sessionId(1L)
+                .description("ANY_SESSION")
+                .duration(3600L)
+                .build();
+        final var firstInputVote = DoVoteInput.builder()
+                .cpf("ANY_CPF")
+                .id(1L)
+                .idSession(1L)
+                .inFavor(TRUE)
+                .build();
+        createSession.execute(sessionInput);
+        Assertions.assertThrows(Exception.class, () -> doVote.execute(firstInputVote));
     }
 }
