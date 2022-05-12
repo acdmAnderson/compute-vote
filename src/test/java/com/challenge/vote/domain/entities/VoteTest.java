508a0b8a2e63f205f9e7933f566bbf9d234a2635
andersonma <anderson.mata@calcard.com.br>
2022-05-11 21:48:49 -0300

    refactor: add static imports

--- src/test/java/com/challenge/vote/domain/entities/VoteTest.java
+++ src/test/java/com/challenge/vote/domain/entities/VoteTest.java
@@ -1,20 +1,20 @@
 package com.challenge.vote.domain.entities;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.time.LocalDateTime;
+import static java.time.LocalDateTime.parse;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 public class VoteTest {
 
     @Test
     void shouldCreateVote() {
-        final var vote = new Vote(1L, 1L,"ANY_CPF", LocalDateTime.parse("2020-01-01T10:00:00"));
-        Assertions.assertEquals(vote.getIdSession(), 1L);
-        Assertions.assertEquals(vote.getId(), 1L);
-        Assertions.assertEquals(vote.getCpf(), "ANY_CPF");
-        Assertions.assertEquals(vote.getCreatedAt(), LocalDateTime.parse("2020-01-01T10:00:00"));
+        final var vote = new Vote(1L, 1L,"ANY_CPF", parse("2020-01-01T10:00:00"));
+        assertEquals(vote.getIdSession(), 1L);
+        assertEquals(vote.getId(), 1L);
+        assertEquals(vote.getCpf(), "ANY_CPF");
+        assertEquals(vote.getCreatedAt(), parse("2020-01-01T10:00:00"));
     }
 }
