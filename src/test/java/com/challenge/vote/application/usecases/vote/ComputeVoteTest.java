a723e4d4ad94155bf87967bc07581a9fa9a63d30
andersonma <anderson.mata@calcard.com.br>
2022-05-12 10:35:25 -0300

    test: ensure vote be computed

---
+++ src/test/java/com/challenge/vote/application/usecases/vote/ComputeVoteTest.java
@@ -0,0 +1,69 @@
+package com.challenge.vote.application.usecases.vote;
+
+import com.challenge.vote.application.usecases.session.CreateSession;
+import com.challenge.vote.application.usecases.session.CreateSessionInput;
+import com.challenge.vote.infra.repositories.memories.SessionRepositoryMemory;
+import com.challenge.vote.infra.repositories.memories.VoteRepositoryMemory;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static java.lang.Boolean.FALSE;
+import static java.lang.Boolean.TRUE;
+
+@SpringBootTest
+public class ComputeVoteTest {
+
+    @Autowired
+    private SessionRepositoryMemory sessionRepository;
+
+    @Autowired
+    private VoteRepositoryMemory voteRepositoryMemory;
+
+    @BeforeEach
+    void setup() {
+        this.sessionRepository.clean();
+        this.voteRepositoryMemory.clean();
+    }
+
+    @Test
+    void ShouldComputeVotes() throws Exception {
+        final var sessionInput = CreateSessionInput.builder()
+                .sessionId(1L)
+                .description("ANY_SESSION")
+                .duration(3600L)
+                .build();
+        final var createSession = new CreateSession(sessionRepository);
+        createSession.execute(sessionInput);
+        final var doVote = new DoVote(sessionRepository, voteRepositoryMemory);
+        final var firstInputVote = DoVoteInput.builder()
+                .cpf("ANY_CPF")
+                .id(1L)
+                .idSession(sessionInput.getSessionId())
+                .inFavor(TRUE)
+                .build();
+        doVote.execute(firstInputVote);
+        final var secondInputVote = DoVoteInput.builder()
+                .cpf("ANOTHER_CPF")
+                .id(2L)
+                .idSession(sessionInput.getSessionId())
+                .inFavor(FALSE)
+                .build();
+        doVote.execute(secondInputVote);
+        final var thirdVoteInput = DoVoteInput.builder()
+                .cpf("SOME_CPF")
+                .id(3L)
+                .idSession(sessionInput.getSessionId())
+                .inFavor(TRUE)
+                .build();
+        doVote.execute(thirdVoteInput);
+        final var computeVote = new ComputeVote(voteRepositoryMemory, sessionRepository);
+        final var output = computeVote.execute(sessionInput.getSessionId());
+        Assertions.assertEquals(output.getSession(), "ANY_SESSION");
+        Assertions.assertEquals(output.getInFavorQuantity(), 2L);
+        Assertions.assertEquals(output.getNotInFavorQuantity(), 1L);
+        Assertions.assertEquals(output.getInFavor(), Boolean.TRUE);
+    }
+}
