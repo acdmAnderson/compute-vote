482a92efd8f0f91af6a7bf6821e0070f59b5888c
acdmAnderson <acdm.anderson@hotmail.com>
2022-05-13 14:06:18 -0300

    refactor: add static imports

--- src/test/java/com/challenge/vote/infra/repositories/databases/VoteRepositoryDatabaseTest.java
+++ src/test/java/com/challenge/vote/infra/repositories/databases/VoteRepositoryDatabaseTest.java
@@ -1,12 +1,13 @@
 package com.challenge.vote.infra.repositories.databases;
 
 import com.challenge.vote.domain.entities.Vote;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.time.LocalDateTime;
+import static java.lang.Boolean.TRUE;
+import static java.time.LocalDateTime.parse;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 public class VoteRepositoryDatabaseTest {
@@ -16,11 +17,11 @@ public class VoteRepositoryDatabaseTest {
 
     @Test
     void shouldCreateVote() {
-        final var vote = new Vote(null, 1L, "ANY_CPF", Boolean.TRUE, LocalDateTime.parse("2021-10-10T10:00:00"));
+        final var vote = new Vote(null, 1L, "ANY_CPF", TRUE, parse("2021-10-10T10:00:00"));
         final var savedVote = this.voteRepositoryDatabase.save(vote);
-        Assertions.assertEquals(savedVote.getSessionId(), 1L);
-        Assertions.assertEquals(savedVote.getCpf(), "ANY_CPF");
-        Assertions.assertEquals(savedVote.getInFavor(), Boolean.TRUE);
-        Assertions.assertEquals(savedVote.getCreatedAt(), LocalDateTime.parse("2021-10-10T10:00:00"));
+        assertEquals(savedVote.getSessionId(), 1L);
+        assertEquals(savedVote.getCpf(), "ANY_CPF");
+        assertEquals(savedVote.getInFavor(), TRUE);
+        assertEquals(savedVote.getCreatedAt(), parse("2021-10-10T10:00:00"));
     }
 }
