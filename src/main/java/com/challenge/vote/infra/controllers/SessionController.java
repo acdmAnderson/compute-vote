c437bab04b044fb09b32aec31db0034252a6d536
acdmAnderson <acdm.anderson@hotmail.com>
2022-05-15 13:17:41 -0300

    feat: ensure SessionController find a Session by id correctly

--- src/main/java/com/challenge/vote/infra/controllers/SessionController.java
+++ src/main/java/com/challenge/vote/infra/controllers/SessionController.java
@@ -1,12 +1,11 @@
 package com.challenge.vote.infra.controllers;
 
-import com.challenge.vote.application.usecases.session.CreateSession;
-import com.challenge.vote.application.usecases.session.CreateSessionInput;
-import com.challenge.vote.application.usecases.session.CreateSessionOutput;
+import com.challenge.vote.application.usecases.session.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.HttpStatus.OK;
 
 @RestController
 @RequestMapping("/v1/session")
@@ -15,9 +14,18 @@ public class SessionController {
     @Autowired
     private CreateSession createSession;
 
+    @Autowired
+    private GetSession getSession;
+
     @PostMapping
     @ResponseStatus(CREATED)
     public CreateSessionOutput create(@RequestBody final CreateSessionInput input) {
         return this.createSession.execute(input);
+    }
+
+    @GetMapping("/{sessionId}")
+    @ResponseStatus(OK)
+    public GetSessionOutput getSessionById(@PathVariable("sessionId") Long sessionId) throws Exception {
+        return this.getSession.execute(sessionId);
     }
 }
