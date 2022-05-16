97c6a1ffcb2646bf475442126fb473a1856766b4
acdmAnderson <acdm.anderson@hotmail.com>
2022-05-16 06:11:18 -0300

    feat: ensure SessionController return 400 when session is already open

--- src/main/java/com/challenge/vote/application/errors/handle/HttpExceptionHandler.java
+++ src/main/java/com/challenge/vote/application/errors/handle/HttpExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.challenge.vote.application.errors.handle;
 
+import com.challenge.vote.application.errors.badrequest.BadRequestException;
 import com.challenge.vote.application.errors.notfound.NotFoundException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
@@ -9,12 +10,13 @@ import org.springframework.web.context.request.ServletWebRequest;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @ControllerAdvice
 public class HttpExceptionHandler extends ResponseEntityExceptionHandler {
 
-    @ExceptionHandler(value = {NotFoundException.class})
+    @ExceptionHandler(NotFoundException.class)
     protected ResponseEntity<Object> handleNotFound(RuntimeException ex, WebRequest webRequest) {
         final var path = ((ServletWebRequest) webRequest).getRequest().getRequestURI();
         final var body = HttpExceptionHandlerOutput.builder()
@@ -27,6 +29,23 @@ public class HttpExceptionHandler extends ResponseEntityExceptionHandler {
                 body,
                 new HttpHeaders(),
                 NOT_FOUND,
+                webRequest
+        );
+    }
+
+    @ExceptionHandler(BadRequestException.class)
+    protected ResponseEntity<Object> handleBadRequest(RuntimeException ex, WebRequest webRequest) {
+        final var path = ((ServletWebRequest) webRequest).getRequest().getRequestURI();
+        final var body = HttpExceptionHandlerOutput.builder()
+                .code(BAD_REQUEST.value())
+                .message(ex.getMessage())
+                .path(path)
+                .build();
+        return this.handleExceptionInternal(
+                ex,
+                body,
+                new HttpHeaders(),
+                BAD_REQUEST,
                 webRequest
         );
     }
