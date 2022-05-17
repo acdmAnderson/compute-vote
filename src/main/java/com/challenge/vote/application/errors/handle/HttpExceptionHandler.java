29a8c1e9c42397eca741d6ae6b063ac2ea9892d4
acdmAnderson <acdm.anderson@hotmail.com>
2022-05-17 10:36:59 -0300

    feat: add log and server error handler

--- src/main/java/com/challenge/vote/application/errors/handle/HttpExceptionHandler.java
+++ src/main/java/com/challenge/vote/application/errors/handle/HttpExceptionHandler.java
@@ -2,6 +2,7 @@ package com.challenge.vote.application.errors.handle;
 
 import com.challenge.vote.application.errors.badrequest.BadRequestException;
 import com.challenge.vote.application.errors.notfound.NotFoundException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -10,10 +11,10 @@ import org.springframework.web.context.request.ServletWebRequest;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import static org.springframework.http.HttpStatus.BAD_REQUEST;
-import static org.springframework.http.HttpStatus.NOT_FOUND;
+import static org.springframework.http.HttpStatus.*;
 
 @ControllerAdvice
+@Slf4j
 public class HttpExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(NotFoundException.class)
@@ -24,6 +25,7 @@ public class HttpExceptionHandler extends ResponseEntityExceptionHandler {
                 .message(ex.getMessage())
                 .path(path)
                 .build();
+        log.error(ex.getMessage(), ex);
         return this.handleExceptionInternal(
                 ex,
                 body,
@@ -41,11 +43,30 @@ public class HttpExceptionHandler extends ResponseEntityExceptionHandler {
                 .message(ex.getMessage())
                 .path(path)
                 .build();
+        log.error(ex.getMessage(), ex);
         return this.handleExceptionInternal(
                 ex,
                 body,
                 new HttpHeaders(),
                 BAD_REQUEST,
+                webRequest
+        );
+    }
+
+    @ExceptionHandler({RuntimeException.class, Exception.class})
+    protected ResponseEntity<Object> handleServerError(RuntimeException ex, WebRequest webRequest) {
+        final var path = ((ServletWebRequest) webRequest).getRequest().getRequestURI();
+        final var body = HttpExceptionHandlerOutput.builder()
+                .code(INTERNAL_SERVER_ERROR.value())
+                .message(ex.getMessage())
+                .path(path)
+                .build();
+        log.error(ex.getMessage(), ex);
+        return this.handleExceptionInternal(
+                ex,
+                body,
+                new HttpHeaders(),
+                INTERNAL_SERVER_ERROR,
                 webRequest
         );
     }
