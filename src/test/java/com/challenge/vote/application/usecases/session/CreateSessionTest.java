be548c4dc0d7898039f26d70a91003e74ce14887
acdmAnderson <acdm.anderson@hotmail.com>
2022-05-17 09:59:40 -0300

    test: ensure CreateSession throw when SessionRepositoryThrows

--- src/test/java/com/challenge/vote/application/usecases/session/CreateSessionTest.java
+++ src/test/java/com/challenge/vote/application/usecases/session/CreateSessionTest.java
@@ -4,9 +4,12 @@ import com.challenge.vote.VoteApplicationTests;
 import com.challenge.vote.infra.repositories.memories.SessionRepositoryMemory;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class CreateSessionTest implements VoteApplicationTests {
 
@@ -28,5 +31,17 @@ public class CreateSessionTest implements VoteApplicationTests {
         createSession.execute(input);
         final var sessions = this.sessionRepository.findAll();
         assertEquals(sessions.size(), 1);
+    }
+
+    @Test
+    void shouldThrow_whenSessionRepositoryThrows() {
+        final var input = CreateSessionInput.builder()
+                .description("ANY_SESSION")
+                .duration(3600L)
+                .build();
+        final var mockRepository = Mockito.mock(SessionRepositoryMemory.class);
+        final var createSession = new CreateSession(mockRepository);
+        Mockito.when(mockRepository.save(ArgumentMatchers.any())).thenThrow(new RuntimeException());
+        assertThrows(RuntimeException.class, () -> createSession.execute(input));
     }
 }
